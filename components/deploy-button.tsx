497b0040b564f010b37e6b3fb6d4748940b2eeb7
sicktastic <antwonlee@gmail.com>
2025-06-27 21:13:50 -0500

    Update homepage

--- components/deploy-button.tsx
+++ components/deploy-button.tsx
@@ -5,7 +5,7 @@ export default function DeployButton() {
   return (
     <>
       <Link
-        href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fnext.js%2Ftree%2Fcanary%2Fexamples%2Fwith-supabase&project-name=nextjs-with-supabase&repository-name=nextjs-with-supabase&demo-title=nextjs-with-supabase&demo-description=This+starter+configures+Supabase+Auth+to+use+cookies%2C+making+the+user%27s+session+available+throughout+the+entire+Next.js+app+-+Client+Components%2C+Server+Components%2C+Route+Handlers%2C+Server+Actions+and+Middleware.&demo-url=https%3A%2F%2Fdemo-nextjs-with-supabase.vercel.app%2F&external-id=https%3A%2F%2Fgithub.com%2Fvercel%2Fnext.js%2Ftree%2Fcanary%2Fexamples%2Fwith-supabase&demo-image=https%3A%2F%2Fdemo-nextjs-with-supabase.vercel.app%2Fopengraph-image.png"
+        href="https://chatgpt.com/g/g-67c5323e64dc8191ad140afe2e2f5d16-robot-combat"
         target="_blank"
       >
         <Button className="flex items-center gap-2" size={"sm"}>
@@ -17,7 +17,7 @@ export default function DeployButton() {
           >
             <path d="M37.5274 0L75.0548 65H0L37.5274 0Z" fill="inherit" />
           </svg>
-          <span>Deploy to Vercel</span>
+          <span>Chat with Combat Robots AI</span>
         </Button>
       </Link>
     </>
