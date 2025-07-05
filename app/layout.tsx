b53433a9bac2e4f4048917b8aec7e12f5b0016cf
sicktastic <antwonlee@gmail.com>
2025-07-05 11:50:36 -0500

    Update community link

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,9 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Combat Robots IO",
+  description:
+    "Your hub for learning everything about combat robots -- from design to competition.",
 };
 
 const geistSans = Geist({
@@ -43,31 +44,13 @@ export default function RootLayout({
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>Combat Robots</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
                   </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                  <DeployButton />
                 </div>
               </nav>
               <div className="flex flex-col gap-20 max-w-5xl p-5">
                 {children}
               </div>
-
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer>
             </div>
           </main>
         </ThemeProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,7 @@ export default async function Home() {
   return (
     <>
       <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
+      <main className="flex-1 flex flex-col gap-6 px-4"></main>
     </>
   );
 }
