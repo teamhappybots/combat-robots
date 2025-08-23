c11afe1b812a0d5092c9d03d755520f893e71803
sicktastic <antwonlee@gmail.com>
2025-08-23 14:12:21 -0500

    chore: Update hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,16 +3,16 @@ export default function Header() {
     <div className="flex flex-col gap-16 items-center">
       <h1 className="sr-only"></h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        Your hub for learning everything about combat robots -- from design to
-        competition.
+        Showcase your combat robot.
       </p>
+      <p>Coming soon.</p>
       <a
-        href="https://community.combatrobots.io"
+        href="https://teamhappybots.com"
         target="_blank"
         className="font-bold hover:underline"
         rel="noreferrer"
       >
-        Go to Community
+        By Team Happy! Bots
       </a>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
